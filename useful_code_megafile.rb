2b0cfddeac14f4da505c3adb332f5f2f59ffca7f
tucker-bower-psjh <tucker.bower@providence.org>
2021-04-28 14:30:50 -0700

    splc solved

--- useful_code_megafile.rb
+++ useful_code_megafile.rb
@@ -65,6 +65,26 @@ def transcribe(dna)
     rna_seq = rna.join('')
 end
 
+def splice_rna(transcript, introns)
+    #remove array of intron strings from transcript string. Spit out spliced transcript comprised of all exons
+    introns_hash = {} #Keys = start position index of intron, values = length of that intron
+    for intron in introns
+        introns_hash[transcript.indices /#{intron}/] = intron.length
+    end
+    
+    exons = []
+    position = 0
+
+    for index in introns_hash.keys.sort
+        exons.append transcript[position..index[0] - 1]
+        position = introns_hash[index] + index[0]
+    end
+
+    exons.append RNA_TRANSCRIPT[position..]
+
+    spliced_transcript = exons.join('')
+end
+
 class String
   def indices e
     start, result = -1, []
@@ -193,6 +213,70 @@ def translate_codons(codons_list)
             print "G"        
         end
     end
+end
+
+def translate(rna_seq)
+    codons_list = []
+    for codon_index in 0..(rna_seq.length / 3 - 1) 
+        codon = ''
+        index_counter = 0
+        for i in 1..3
+            codon += rna_seq[(codon_index * 3 + index_counter)]
+            index_counter += 1
+        end
+        codons_list.append(codon)
+    end
+
+    amino_acids = []
+    for codon in codons_list
+        case codon
+        when "UUU", "UUC"
+            amino_acids.append "F"
+        when "UUA", "UUG", "CUU", "CUC", "CUA", "CUG"
+            amino_acids.append "L"
+        when "UCU", "UCC", "UCA", "UCG" 
+            amino_acids.append "S"
+        when "UAU", "UAC"
+            amino_acids.append "Y"
+        when "UAA", "UAG", "UGA"
+            break #stop codon
+        when "UGU", "UGC"
+            amino_acids.append "C"
+        when "UGG"
+            amino_acids.append "W"
+        when "CCU", "CCC", "CCA", "CCG"
+            amino_acids.append "P"
+        when "CAU", "CAC"
+            amino_acids.append "H"
+        when "CAA", "CAG"
+            amino_acids.append "Q"
+        when "CGU", "CGC", "CGA", "CGG", "AGA", "AGG"
+            amino_acids.append "R"
+        when "AUU", "AUC", "AUA"
+            amino_acids.append "I"
+        when "AUG"
+            amino_acids.append "M"
+        when "ACU", "ACC", "ACA", "ACG"
+            amino_acids.append "T"
+        when "AAU", "AAC"
+            amino_acids.append "N"
+        when "AAA", "AAG"
+            amino_acids.append "K"
+        when "AGU", "AGC"
+            amino_acids.append "S"
+        when "GUU", "GUC", "GUA", "GUG"
+            amino_acids.append "V"
+        when "GCU", "GCC", "GCA", "GCG"
+            amino_acids.append "A"
+        when "GAU", "GAC"
+            amino_acids.append "D"
+        when "GAA", "GAG"
+            amino_acids.append "E"
+        when "GGU", "GGC", "GGA", "GGG"
+            amino_acids.append "G"        
+        end
+    end
+    return amino_acids.join('')
 end
 
 def gc_content(fasta_hash)
