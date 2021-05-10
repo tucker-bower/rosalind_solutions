1edf7e0b1b39c220187e083b4841d83990a05556
tucker-bower-psjh <tucker.bower@providence.org>
2021-05-09 18:00:58 -0700

    improved fib & mrna code clarity w/ rubocop

--- fib/fib_solver.rb
+++ fib/fib_solver.rb
@@ -3,9 +3,8 @@
 nk_file = 'rosalind_fib.txt'
 nk = File.readlines(nk_file)[0]
 nandk = nk.split
-#could split on whitespace to one liner get these values
+# could split on whitespace to one liner get these values
 nk_re = nk.match /([0-9]+)\s([0-9]+)/
-
 
 n = nk_re.captures[0].to_i
 k = nk_re.captures[1].to_i
@@ -15,7 +14,7 @@ puts ''
 print 'k = ', k, '(offspring per generation)'
 puts ''
 
-rabbits_seq = [1,1]
+rabbits_seq = [1, 1]
 
 for i in 2..n-1
     rabbits_seq.append (rabbits_seq[i-2]*k + rabbits_seq[i-1])
@@ -23,4 +22,3 @@ end
 
 print 'rabbits after n generations = ', rabbits_seq[rabbits_seq.length-1]
 puts ''
-
